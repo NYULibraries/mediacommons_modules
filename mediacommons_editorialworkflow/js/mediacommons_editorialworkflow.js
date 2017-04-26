0d77e1d6ae97cfcf63e7e85355ecebecbfb1038e
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-04-26 16:26:29 -0400

    MC-247-1

--- mediacommons_editorialworkflow/js/mediacommons_editorialworkflow.js
+++ mediacommons_editorialworkflow/js/mediacommons_editorialworkflow.js
@@ -1,49 +1,25 @@
-/**
- * Implementation of Drupal behavior.
- */
-
 (function($) {
-  
-    Drupal.behaviors.mediacommons_editorialworkflow = {};
-  
-    Drupal.behaviors.mediacommons_editorialworkflow.attach = function(context) {
-        if (window.console) console.log('attach mediacommons_editorialworkflow');
-        
-        /** Hide messages */
-        
-        /**
-        setTimeout(function() {
-          $('#console').hide();
-        }, 3000);
-        */
-       
-        $(".field-name-field-attached-images .image-preview img").not('.mediacommons_editorialworkflow_proceeded').each(function() {
-            var currentTarget = $(this),
-                targetArea = currentTarget.parent(),
-                originalTarget = targetArea.next().children('span').children('a'),
-                currentTargetSource = currentTarget.attr('src'),
-                originalTargetSource = originalTarget.attr('href');
-                currentTarget.addClass('mediacommons_editorialworkflow_proceeded');               
-                targetArea.append('<div class="image-preview-actions"><a href="#crop" class="crop">crop</a> <a href="#remove" class="remove mdelete">remove</a></div>');               
-                currentTarget.removeAttr('width');
-                currentTarget.removeAttr('height');
-                currentTarget.attr({ 'data-fullsize': originalTargetSource, 'data-preview': currentTargetSource });
-        });
-       
-        $("body").delegate("#edit-field-type input:radio", "change", function(e) {
-            var type = parseInt($(this).val(), 10);
-            $('.field-name-field-video-embed-link, .field-name-field-representative-image, .field-name-field-tease-image').toggle();
-        });
-        
-        $(".column-side").delegate(".image-preview img", "mouseenter", function(e) {
-            var currentTarget = $(this);
-            currentTarget.attr('src', currentTarget.attr('data-fullsize'));
-        });
-        
-        $(".column-side").delegate(".image-preview img", "mouseleave", function(e) {
-            currentTarget = $(this);
-            currentTarget.attr('src', currentTarget.attr('data-preview'));
-        });
-
-  };  
+  Drupal.behaviors.mediacommons_editorialworkflow = {
+    attach: function (context, settings) {
+      if (settings.mediacommons_editorialworkflow) {
+        if (settings.mediacommons_editorialworkflow.isAdminPage) {
+          if (settings.mediacommons_editorialworkflow.redirect.trigger) {
+            setInterval(function() {
+              var node = $(context).find('#console .redirect');
+              var count = parseInt(node.text(), 10) - 1;
+              if (count => 0) {
+                node.text(count);
+              }
+              if (count === 0) {
+                window.location.replace(settings.mediacommons_editorialworkflow.redirect.path);
+              }
+            }, 995);
+          }
+          setTimeout(function() {
+            $(context).find('#console').hide();
+          }, 6000);
+        }
+      }
+    }
+  }
 })(jQuery);
