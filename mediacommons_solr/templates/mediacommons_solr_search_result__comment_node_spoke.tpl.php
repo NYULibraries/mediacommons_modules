105557d96e42de45081f48212e8deb2ecb0ce5ce
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-12-08 23:18:29 -0500

    update

--- mediacommons_solr/templates/mediacommons_solr_search_result__comment_node_spoke.tpl.php
+++ mediacommons_solr/templates/mediacommons_solr_search_result__comment_node_spoke.tpl.php
@@ -1,12 +1,11 @@
 <li class="<?php print $classes ?>">
   <div class="field-name-field-project"><?php print $project_link ?></div>
   <div class="referenced-node">
-    <span class="views-label">Re: </span><a href="#">Data needed: Referenced Spoke</a>
+    <span class="views-label">Re: </span><a href="<?php print $referencedspoke->url ?>" data-nid="<?php print $referencedspoke->nid ?>"><?php print $referencedspoke->label ?></a>
   </div>
   <h3 class="comment__title comment-title">
     <a href="<?php print $url ?>" class="permalink" ><?php print $title; ?></a>
   </h3>
-  <!-- TODO Data needed: date comment was created, in this format:  Monday, October 23, 2017 — 7:46 pm -->
   <time datetime="YYYY-MM-DD"><?php print $created_date ?></time>
   <div class="comment-body">
     <?php print $snippet; ?>
