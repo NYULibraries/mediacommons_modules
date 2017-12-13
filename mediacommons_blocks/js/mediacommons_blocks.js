57552d52fa9f86c2e05f8e86ce23a71f87cadafc
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-12-13 10:55:43 -0500

    infinite scroll

--- mediacommons_blocks/js/mediacommons_blocks.js
+++ mediacommons_blocks/js/mediacommons_blocks.js
@@ -3,8 +3,15 @@
   Drupal.behaviors.mediacommons_blocks = {
     attach : function(context, settings) {
       var $pager = $('.mc-mc .pager', context);
-      if ($pager.find('a').length) {	
-        // https://infinite-scroll.com/
+      var $links = $pager.find('a');
+      if ($links.length) {
+    	$links.each(function() {
+    	  var $href = $(this).attr('href');
+    	  var bjax = $href + '&bjax=1&pjax=1';
+    	  $(this).attr('href', bjax);
+    	});
+        // https://infinite-scroll.com
+    	  // https://infinite-scroll.com/events.html
     	var $container = $('.mediacommons_solr-results').infiniteScroll({
           path: '.pager-next a',
           loadOnScroll: true,
@@ -13,9 +20,9 @@
           // status: '.scroller-status',
           hideNav: '.mediacommons_solr-results .pager'
         });
-        //$container.on( 'load.infiniteScroll', function(event, response) {
-          //console.log(response);
-        //});
+    	$container.on('load.infiniteScroll', function(event, response, path) {
+          console.log(path);
+        });
       }
     }
   }
