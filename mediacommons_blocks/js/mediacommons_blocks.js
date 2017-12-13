8f01345031a923df18de7b442f54330f0c0c26a2
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-12-13 09:40:57 -0500

    infinite-scroll

--- mediacommons_blocks/js/mediacommons_blocks.js
+++ mediacommons_blocks/js/mediacommons_blocks.js
@@ -1,23 +1,21 @@
 ;(function ($) {
   "use strict";
-  function getParameterByName (name, url) {
-    if (!url) return null;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
   Drupal.behaviors.mediacommons_blocks = {
     attach : function(context, settings) {
       var $pager = $('.mc-mc .pager', context);
-      if ($pager.find('a').length) {
+      if ($pager.find('a').length) {	
         // https://infinite-scroll.com/
-        $('.mediacommons_solr-results').infiniteScroll({
+    	var $container = $('.mediacommons_solr-results').infiniteScroll({
           path: '.pager-next a',
-          append: '.search-result',
-          history: false,
+          loadOnScroll: true,
+          history: false,          
+          append: '.mediacommons_solr-results',
+          // status: '.scroller-status',
+          hideNav: '.mediacommons_solr-results .pager'
         });
+        //$container.on( 'load.infiniteScroll', function(event, response) {
+          //console.log(response);
+        //});
       }
     }
   }
