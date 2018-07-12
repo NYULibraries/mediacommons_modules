9600f28c0b8f9b4c8f3e9321a907f72605fcc56e
Alberto Ortiz Flores <aortiz@nyu.edu>
2018-07-12 18:59:25 +0000

    added test for JIRA ticket MC-447

--- nightwatch/test/mc-447/index.js
+++ nightwatch/test/mc-447/index.js
@@ -1,6 +1,27 @@
 var conf = require('../../nightwatch.conf.js');
 
 module.exports = {
+
+  'MC-447 - Attempting to comment leads to page not found' : function (client) {
+    client
+      .url('http://mediacommons.local/imr/2007/12/13/citizens-media-in-southern-colombia')
+      .saveScreenshot('1.png')
+      .waitForElementVisible('#comments', 1000)
+      .assert.containsText('#comments article .links li span a:first-child', 'Log in')
+      .click('#comments article .links li span a:first-child')
+      .assert.urlContains('/user?project=imr')
+      .saveScreenshot('2.png')
+      .waitForElementVisible('form#user-login', 1000)
+      .setValue('input#edit-name', 'mc.contributor.1')
+      .setValue('input#edit-pass', '000000')
+      .saveScreenshot('3.png')
+      .submitForm('#user-login')
+      .waitForElementVisible('body', 1000)
+      .saveScreenshot('4.png')
+      .assert.urlContains('/imr/node/461')
+      .assert.containsText('header.comments-add h2.comments__form-title', 'Add new comment')
+      .end();
+  },
   
   'MC-447 - Click username link in a serch result should take you that user profile' : function (client) {
       client
@@ -14,11 +35,12 @@ module.exports = {
   },
   
   'Finished': function (client) {
-      client
-        .perform(() => {
-          console.log('[perform]: Finished Test:', client.currentTest.name)
-        })
-        .end();
+    client
+      .url('http://mediacommons.local/user/logout')      
+      .perform(() => {
+        console.log('[perform]: Finished Test:', client.currentTest.name)
+      })
+      .end();
   }       
 
 };
